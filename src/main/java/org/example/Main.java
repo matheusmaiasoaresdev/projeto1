e9b878fbf7f917153671faad6e3e474ffcab1783
matheusmaiasoaresdev <126512900+matheusmaiasoaresdev@users.noreply.github.com>
2023-02-27 21:57:29 -0300

    fiz a alteracao devido a uma instabildade.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(" Boa tarde ");
+        System.out.println(" vamos testar essa alteracao ");
     }
 }
